2751a27ff059355062fb8299f16070acfba2a57c
PasanBistec <pasan@bistecglobal.com>
2022-10-27 15:08:53 +0530

    Two components share the same code

---
+++ src/components/DocTitleOne.js
@@ -0,0 +1,19 @@
+import React ,{useState,useEffect} from 'react'
+
+function DocTitleOne() {
+ 
+    const[count,setCount] = useState(0)
+
+
+    useEffect(()=>{
+      document.title = `Count ${count}`
+    },[count])
+
+  return (
+    <div>
+        <button onClick={()=> setCount(count+1)}>count {count}</button>
+    </div>
+  )
+}
+
+export default DocTitleOne
